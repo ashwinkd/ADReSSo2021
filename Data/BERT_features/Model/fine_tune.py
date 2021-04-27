759534e39cb9050990dd452eddcf8f25c235bb4d
Ashwin <ashwindeshpande96@gmail.com>
2021-04-27 02:16:46 -0500

    BERT features

---
+++ Data/BERT_features/Model/fine_tune.py
@@ -0,0 +1,167 @@
+from Data.BERT_features.Model.model import *
+from Data.BERT_features.global_vars import *
+from Data.BERT_features.prep_data import *
+
+bert = AutoModel.from_pretrained('bert-base-uncased')
+
+for param in bert.parameters():
+    param.requires_grad = False
+
+# pass the pre-trained BERT to our define architecture
+model = BERT_Arch(bert)
+
+# push the model to GPU
+model = model.to(device)
+
+optimizer = AdamW(model.parameters(), lr=1e-3)
+
+# compute the class weights
+class_wts = compute_class_weight('balanced', np.unique(y_train), y_train)
+
+weights = torch.tensor(class_wts, dtype=torch.float)
+weights = weights.to(device)
+
+# loss function
+cross_entropy = nn.NLLLoss(weight=weights)
+
+# number of training epochs
+epochs = 10
+
+
+# function to train the model
+def train():
+    model.train()
+
+    total_loss, total_accuracy = 0, 0
+
+    # empty list to save model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step, batch in enumerate(train_dataloader):
+
+        # progress update after every 50 batches.
+        if step % 50 == 0 and not step == 0:
+            print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(train_dataloader)))
+
+        # push the batch to gpu
+        batch = [r.to(device) for r in batch]
+
+        sent_id, mask, labels = batch
+
+        # clear previously calculated gradients
+        model.zero_grad()
+
+        # get model predictions for the current batch
+        preds = model(sent_id, mask)
+
+        # compute the loss between actual and predicted values
+        loss = cross_entropy(preds, labels)
+
+        # add on to the total loss
+        total_loss = total_loss + loss.item()
+
+        # backward pass to calculate the gradients
+        loss.backward()
+
+        # clip the the gradients to 1.0. It helps in preventing the exploding gradient problem
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        # update parameters
+        optimizer.step()
+
+        # model predictions are stored on GPU. So, push it to CPU
+        preds = preds.detach().cpu().numpy()
+
+        # append the model predictions
+        total_preds.append(preds)
+
+    # compute the training loss of the epoch
+    avg_loss = total_loss / len(train_dataloader)
+
+    # predictions are in the form of (no. of batches, size of batch, no. of classes).
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds = np.concatenate(total_preds, axis=0)
+
+    # returns the loss and predictions
+    return avg_loss, total_preds
+
+
+# function for evaluating the model
+def evaluate():
+    print("\nEvaluating...")
+
+    # deactivate dropout layers
+    model.eval()
+
+    total_loss, total_accuracy = 0, 0
+
+    # empty list to save the model predictions
+    total_preds = []
+
+    # iterate over batches
+    for step, batch in enumerate(val_dataloader):
+
+        # Progress update every 50 batches.
+        if step % 50 == 0 and not step == 0:
+            # Report progress.
+            print('  Batch {:>5,}  of  {:>5,}.'.format(step, len(val_dataloader)))
+
+        # push the batch to gpu
+        batch = [t.to(device) for t in batch]
+
+        sent_id, mask, labels = batch
+
+        # deactivate autograd
+        with torch.no_grad():
+
+            # model predictions
+            preds = model(sent_id, mask)
+
+            # compute the validation loss between actual and predicted values
+            loss = cross_entropy(preds, labels)
+
+            total_loss = total_loss + loss.item()
+
+            preds = preds.detach().cpu().numpy()
+
+            total_preds.append(preds)
+
+    # compute the validation loss of the epoch
+    avg_loss = total_loss / len(val_dataloader)
+
+    # reshape the predictions in form of (number of samples, no. of classes)
+    total_preds = np.concatenate(total_preds, axis=0)
+
+    return avg_loss, total_preds
+
+
+# set initial loss to infinite
+best_valid_loss = float('inf')
+
+# empty lists to store training and validation loss of each epoch
+train_losses = []
+valid_losses = []
+
+# for each epoch
+for epoch in range(epochs):
+
+    print('\n Epoch {:} / {:}'.format(epoch + 1, epochs))
+
+    # train model
+    train_loss, _ = train()
+
+    # evaluate model
+    valid_loss, _ = evaluate()
+
+    # save the best model
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'saved_weights.pt')
+
+    # append training and validation loss
+    train_losses.append(train_loss)
+    valid_losses.append(valid_loss)
+
+    print(f'\nTraining Loss: {train_loss:.3f}')
+    print(f'Validation Loss: {valid_loss:.3f}')
