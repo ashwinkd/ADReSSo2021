cb1646d290d9a4dac156640d593704382479172d
ashwinkd <adeshp27@uic.edu>
2021-05-13 00:35:15 -0500

    csv fixed

--- ADReSSo20/create_csv.py
+++ ADReSSo20/create_csv.py
@@ -4,39 +4,54 @@ import pandas as pd
 
 dys_and_parse = pd.read_pickle('all_tags.pkl')
 idx = 0
+keys = set()
 
 
 def get_key(text):
-    text = text.replace("_", "")
-    text = text.replace("E", "")
-    text = re.sub(r"\s", "", text).strip()
-    return text.lower()
+    text = text.split()
+    twords = text[::2]
+    text = ""
+    for t in twords:
+        t = normalize(t)
+        if t.startswith("'") or t.startswith("n'"):
+            text += t
+        else:
+            text += " " + t
+    return text.strip()
+
+
+def normalize(text):
+    text = text.lower()
+    text = re.sub(r"[^a-z' ]", "", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
 
 
 # parse_tree = open('adresso20_text_dys.txt', 'r').readlines()
-dys_and_parse['key'] = dys_and_parse.disfluency.apply(lambda x: get_key(x))
-dys_and_parse = dys_and_parse.set_index('key').T.to_dict('list')
+dys_and_parse['utterance'] = dys_and_parse.disfluency.apply(lambda x: get_key(x))
+dys_and_parse = dys_and_parse.set_index('utterance').T.to_dict('list')
 
 data = pd.read_pickle('transcript_with_disfluency_parse.pickle')
 
 
 def get_disfluency(utterance):
-    ukey = get_key(utterance)
+    utterance_normal = normalize(utterance)
     try:
-        _, disfluency = dys_and_parse[ukey]
+        _, disfluency = dys_and_parse[utterance_normal]
+        keys.add(utterance_normal)
         return disfluency
     except:
-        print(utterance)
+        if utterance:
+            print(utterance, utterance_normal)
         return None
 
 
 def get_parse(utterance):
-    ukey = get_key(utterance)
+    utterance_normal = normalize(utterance)
     try:
-        parse, _ = dys_and_parse[ukey]
+        parse, _ = dys_and_parse[utterance_normal]
         return parse
     except:
-        print(utterance)
         return None
 
 
@@ -51,3 +66,8 @@ data[['speaker',
       'disfluency_text',
       'parse_tree',
       'recall']].to_pickle('transcript_with_disfluency_parse.pickle')
+print("#" * 20)
+for k in keys:
+    dys_and_parse.pop(k)
+for (key, (parse, dtext)) in dys_and_parse.items():
+    print(dtext)
